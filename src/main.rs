87d3196c7e819ade074fc6863696725ddb60ded5
Valera <108022376+kekobka@users.noreply.github.com>
2024-07-24 14:26:14 +0700

    delete unused files

--- src/main.rs
+++
@@ -1,8 +0,0 @@
-// for tests
-fn main() {
-    let input = include_bytes!("../cube.obj");
-    let model: wavefront::Obj = wavefront::Obj::from_reader(&input[..]).unwrap();
-    for (name, _obj) in model.objects() {
-        print!("{}\n", name);
-    }
-}
